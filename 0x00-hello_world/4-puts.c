9c754839a50d5be9845be61cf9ffe16df5ccf191
kevin <k.lienard@yahoo.fr>
2021-09-30 10:25:29 +0000

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /*
  * DESCRIPTION:
  *
- * Write a C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line
+ * Programming is like building a multilingual puzzle
  *
  */
 
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /* DESCRIPTION:
  *
- * Whrite a C program that prints exactly with proper grammar, but the outcome is a piece of art, followed by a new lin
+ * with proper grammar, but the outcome is a piece of art
  *
  */
 
