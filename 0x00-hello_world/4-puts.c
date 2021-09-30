243782810e8cad7b7f53c10e1b968045dcbd916f
kevin <k.lienard@yahoo.fr>
2021-09-30 11:33:51 +0000

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * DESCRIPTION:
+ * MAIN: La fonction main d’un programme C est la première fonction exécutée lors du lancement du programme
+ *  
+ * DESCRIPTION: Programming is like building a multilingual puzzle
  *
- * Programming is like building a multilingual puzzle
- *
+ * RETURN: return met fin à l’exécution d’une fonction et retourne le contrôle à la fonction appelante
  */
 int main(void)
 {
