8ad65a8b565a10355de28235d65a2cd31c9b33a5
kevin <k.lienard@yahoo.fr>
2021-09-30 11:27:26 +0000

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-/*
+/**
  * DESCRIPTION:
  *
  * Programming is like building a multilingual puzzle
  *
  */
-
 int main(void)
-
 {
 		puts("\"Programming is like building a multilingual puzzle");
 		return (0);
