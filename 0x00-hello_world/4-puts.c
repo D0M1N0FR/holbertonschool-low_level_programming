3faa05faf8e31203e35839f6c37836d1fdc1752c
kevin <k.lienard@yahoo.fr>
2021-09-30 10:18:00 +0000

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-/* DESCRIPTION:
+/*
+ * DESCRIPTION:
  *
  * Write a C program that prints exactly "Programming is like building a multilingual puzzle", followed by a new line.
  */
