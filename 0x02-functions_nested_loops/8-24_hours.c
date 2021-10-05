ab3c42ac8d8a077b8b27b0de44a5bca8eb8815b2
kevin <k.lienard@yahoo.fr>
2021-10-05 17:39:12 +0000

    test

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,49 +1,38 @@
 #include "main.h"
-
 /**
- * jack_bauer - prints every minute of the day
- *
- * Description: prints every minute of the day
- *
- * Return: void
+ * jack_bauer - Print every minute of the day of Jack Bauer
+ * Description: Start from 00:00 to 23:59
  */
-
 void jack_bauer(void)
 {
-	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
+	int x = 0;
+	int a = 0, b = 0, c = 0, d = 0;
 
-	hours_max = 58;
-	hours_tens = '0';
-	while (hours_tens < '3')
+	while (x < 1440)
 	{
-		if (hours_tens == '2')
+		_putchar(a + '0');
+		_putchar(b + '0');
+		_putchar(':');
+		_putchar(c + '0');
+		_putchar(d + '0');
+		_putchar('\n');
+
+		d++;
+		if (d > 9)
 		{
-			hours_max = '4';
+			d = 0;
+			c++;
 		}
-		hours_ones = '0';
-		while (hours_ones < hours_max)
+		if (c > 5)
 		{
-			minutes_tens = '0';
-			while (minutes_tens < '6')
-			{
-				minutes_ones = '0';
-				while (minutes_ones < 58)
-				{
-					_putchar(hours_tens);
-					_putchar(hours_ones);
-					_putchar(':');
-					_putchar(minutes_tens);
-					_putchar(minutes_ones);
-					_putchar('\n');
-					minutes_ones++;
-				}
-				minutes_ones = '0';
-				minutes_tens++;
-			}
-			minutes_tens = '0';
-			hours_ones++;
+			c = 0;
+			b++;
 		}
-		hours_ones = '0';
-		hours_tens++;
+		if (b > 9)
+		{
+			b = 0;
+			a++;
+		}
+		x++;
 	}
 }
