705bd16cb0c70ce40f559b91d79a82219e4cf398
kevin <k.lienard@yahoo.fr>
2021-10-06 08:58:42 +0000

    test

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,51 +1,32 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
-  * print_to_98 - Prints all natural numbers from n to 98
-  * @n: Starting number to print all the way to 98
-  *
-  */
+ * print_to_98 - prints numbers to 98
+ * @n: starting integer
+ *
+ * Description: prints all natural number from n - 98
+ *
+ * Return: void
+ */
 
 void print_to_98(int n)
 {
-	if (n >= 98 && n > 0)
+	if (n < 98)
 	{
-		while (n >= 98)
+		while (n < 98)
 		{
-			if (n >= 100)
-				_putchar(n / 100 + '0');
-			_putchar(n / 10 % 10 + '0');
-			_putchar(n % 10 + '0');
-			if (n != 98)
-				_putchar(',');
-			if (n != 98)
-				_putchar(' ');
-			n--;
-		}
-	}
-	else
-	{
-		while (n <= 98)
-		{
-			if (n < 0)
-			{
-				_putchar('-');
-				if (n <= -100)
-					_putchar((n - (n * 2)) / 100 + '0');
-				if (n <= -10)
-					_putchar((n - (n * 2)) / 10 % 10 + '0');
-				_putchar((n - (n * 2)) % 10 + '0');
-			}
-			if (n >= 10)
-				_putchar(n / 10 + '0');
-			if (n >= 0)
-				_putchar(n % 10 + '0');
-			if (n != 98)
-				_putchar(',');
-			if (n != 98)
-				_putchar(' ');
+			printf("%d, ", n);
 			n++;
 		}
 	}
-	_putchar('\n');
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	printf("98\n");
 }
