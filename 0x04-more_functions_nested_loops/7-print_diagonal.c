f68034c888e31c6f7335c98daddda563d99efadf
kevin <k.lienard@yahoo.fr>
2021-10-07 13:01:54 +0000

    test

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,27 +9,23 @@
 
 void print_diagonal(int n)
 {
-	int line = 0;
-	int spaces;
+	int c, i;
 
-	if (n > 0)
+	c = 0;
+
+	while (n > 0)
 	{
-		while (line < n)
+		i = c;
+		while (i > 0)
 		{
-			_putchar('\n');
-			for (spaces = 0; spaces < line; spaces++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('$');
-			line++;
+			_putchar(' ');
+			i--;
 		}
-	}
-	else
-	{
+		_putchar('\\');
 		_putchar('\n');
-		_putchar('$');
-		_putchar('\n');
+		c++;
+		n--;
 	}
+	if (c < 1)
+		_putchar('\n');
 }
